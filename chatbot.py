5074a514182512dbd3b8999bd3bd7e7feb6f1b59
Mariamshifaya <mariamshifaya2025@gmail.com>
2026-03-08 14:49:02 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,10 +3,10 @@ import json
 import pickle
 import random
 
-model = pickle.load(open("/content/drive/MyDrive/NLP_Chatbot_Project/model.pkl","rb"))
-vectorizer = pickle.load(open("/content/drive/MyDrive/NLP_Chatbot_Project/vectorizer.pkl","rb"))
+model = pickle.load(open("model.pkl","rb"))
+vectorizer = pickle.load(open("vectorizer.pkl","rb"))
 
-with open("/content/drive/MyDrive/NLP_Chatbot_Project/intents.json") as file:
+with open("intents.json") as file:
     data = json.load(file)
 
 def get_response(user_input):
