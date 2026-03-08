461952ca5132cb0f756eef5a1ec6ed9666775098
Mariamshifaya <mariamshifaya2025@gmail.com>
2026-03-08 16:18:38 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,26 @@ import pickle
 model = pickle.load(open("model.pkl","rb"))
 vectorizer = pickle.load(open("vectorizer.pkl","rb"))
 
+intents = {
+ "intents":[
+   {
+     "tag":"greeting",
+     "patterns":["Hi","Hello","Hey"],
+     "responses":["Hello!","Hi there!"]
+   },
+   {
+     "tag":"goodbye",
+     "patterns":["Bye","See you"],
+     "responses":["Goodbye!","See you later"]
+   },
+   {
+     "tag":"thanks",
+     "patterns":["Thanks","Thank you"],
+     "responses":["You're welcome"]
+   }
+ ]
+}
+
 def get_response(user_input):
 
     vec = vectorizer.transform([user_input])
